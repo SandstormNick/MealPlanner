0884f67ddb8ccd6561416951b3e9af8f86ae3796
SandstormNick <gibbensnick@gmail.com>
2024-01-21 14:33:58 +0200

    Finished DB - Iteration 1 at least

--- meal_planner/lib/helpers/db_helper.dart
+++ meal_planner/lib/helpers/db_helper.dart
@@ -14,13 +14,12 @@ class DBHelper {
     await db.execute('CREATE TABLE day(DayId INTEGER PRIMARY KEY, DayName TEXT)');
     await db.execute('CREATE TABLE mealtime(MealTimeId INTEGER PRIMARY KEY, MealTimeName TEXT)');
     await db.execute('CREATE TABLE day_mealtime(DayMealTimeId INTEGER PRIMARY KEY, DayId_FK INTEGER, MealTimeId_FK INTEGER, FOREIGN KEY (DayId_FK) REFERENCES day(DayId), FOREIGN KEY (MealTimeId_FK) REFERENCES mealtime(MealTimeId))');
-    //DAY
-      //DAY_MEALTIME
-    //MEALTIME
-        //MEALTIME_FOOD
-    //MEAL
-      //MEAL_INGREDIANT
-    //INGREDIANT
-    //SHOPPINGLIST
+    await db.execute('CREATE TABLE meal(MealId INTEGER PRIMARY KEY, MealName TEXT, IsDeleted INTEGER DEFAULT 0)');
+    await db.execute('CREATE TABLE ingredient(IngredientId INTEGER PRIMARY KEY, IngredientName TEXT, IsDeleted INTEGER DEFAULT 0)');
+    await db.execute('CREATE TABLE meal_ingredient(MealIngredientId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId))');
+    await db.execute('CREATE TABLE shopping_list(ShoppingListItemId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, IsMeal INTEGER, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId))');
+
+    //This is the main joining table that links meals/ingredients with the day_mealtime table and displays on the main Week screen
+    await db.execute('CREATE TABLE mealtime_food(MealTimeFoodId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, DayMealTimeId_FK, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId), FOREIGN KEY (DayMealTimeId_FK) REFERENCES day_mealtime(DayMealTimeId))');
   }
 }
