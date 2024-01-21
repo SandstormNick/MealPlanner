72eea58cf5200c603bc1a2e542767602af175a67
SandstormNick <gibbensnick@gmail.com>
2024-01-21 16:05:46 +0200

    Added home screen and added navigation bar widget

---
+++ meal_planner/lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../widgets/navigation_bar.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    Screen1(),
+    Screen2(),
+    Screen3(),
+    Screen4(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'), //Will change this at some point
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: AppNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ meal_planner/lib/widgets/navigation_bar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AppNavigationBar extends ConsumerWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const AppNavigationBar({Key? key, required this.currentIndex, required this.onTap}) : super(key:key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.grey,
+      backgroundColor: Colors.white,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          label: 'Week',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.restaurant_menu),
+          label: 'Meals',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.kitchen),
+          label: 'Ingredients',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_cart),
+          label: 'Shopping List',
+        ),
+      ],
+    );
+  }
+}
+
+
+//---------------------------------------------------------Temporary
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Week Screen'),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Meals Screen'),
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Ingredients Screen'),
+    );
+  }
+}
+
+class Screen4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Shopping List Screen'),
+    );
+  }
+}
