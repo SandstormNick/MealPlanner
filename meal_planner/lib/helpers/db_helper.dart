11d6eb7872ff40172881c355ab01a4d008d3832c
SandstormNick <gibbensnick@gmail.com>
2024-01-28 21:01:55 +0200

    Added new getDataRawQuery method in DBHelper class

--- meal_planner/lib/helpers/db_helper.dart
+++ meal_planner/lib/helpers/db_helper.dart
@@ -3,28 +3,36 @@ import 'package:path/path.dart' as path;
 import 'package:sqflite/sqlite_api.dart';
 
 import '/helpers/seed_helper.dart';
+import '/helpers/raw_sql_query_helper.dart';
 
 class DBHelper {
   static Future<Database> database() async {
     final dbPath = await sql.getDatabasesPath();
     return sql.openDatabase(path.join(dbPath, 'mealplanner.db'),
-      onCreate: (db, version) => _createDb(db), version: 1
-    );
+        onCreate: (db, version) => _createDb(db), version: 1);
   }
 
   static void _createDb(Database db) async {
-    await db.execute('CREATE TABLE day(DayId INTEGER PRIMARY KEY, DayName TEXT)');
-    await db.execute('CREATE TABLE mealtime(MealTimeId INTEGER PRIMARY KEY, MealTimeName TEXT)');
-    await db.execute('CREATE TABLE day_mealtime(DayMealTimeId INTEGER PRIMARY KEY, DayId_FK INTEGER, MealTimeId_FK INTEGER, FOREIGN KEY (DayId_FK) REFERENCES day(DayId), FOREIGN KEY (MealTimeId_FK) REFERENCES mealtime(MealTimeId))');
-    await db.execute('CREATE TABLE meal(MealId INTEGER PRIMARY KEY, MealName TEXT, IsDeleted INTEGER DEFAULT 0)');
-    await db.execute('CREATE TABLE ingredient(IngredientId INTEGER PRIMARY KEY, IngredientName TEXT, IsDeleted INTEGER DEFAULT 0)');
-    await db.execute('CREATE TABLE meal_ingredient(MealIngredientId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId))');
-    await db.execute('CREATE TABLE shopping_list(ShoppingListItemId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, IsMeal INTEGER, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId))');
+    await db
+        .execute('CREATE TABLE day(DayId INTEGER PRIMARY KEY, DayName TEXT)');
+    await db.execute(
+        'CREATE TABLE mealtime(MealTimeId INTEGER PRIMARY KEY, MealTimeName TEXT)');
+    await db.execute(
+        'CREATE TABLE day_mealtime(DayMealTimeId INTEGER PRIMARY KEY, DayId_FK INTEGER, MealTimeId_FK INTEGER, FOREIGN KEY (DayId_FK) REFERENCES day(DayId), FOREIGN KEY (MealTimeId_FK) REFERENCES mealtime(MealTimeId))');
+    await db.execute(
+        'CREATE TABLE meal(MealId INTEGER PRIMARY KEY, MealName TEXT, IsDeleted INTEGER DEFAULT 0)');
+    await db.execute(
+        'CREATE TABLE ingredient(IngredientId INTEGER PRIMARY KEY, IngredientName TEXT, IsDeleted INTEGER DEFAULT 0)');
+    await db.execute(
+        'CREATE TABLE meal_ingredient(MealIngredientId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId))');
+    await db.execute(
+        'CREATE TABLE shopping_list(ShoppingListItemId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, IsMeal INTEGER, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId))');
 
     //This is the main joining table that links meals/ingredients with the day_mealtime table and displays on the main Week screen
-    await db.execute('CREATE TABLE mealtime_food(MealTimeFoodId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, DayMealTimeId_FK, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId), FOREIGN KEY (DayMealTimeId_FK) REFERENCES day_mealtime(DayMealTimeId))');
+    await db.execute(
+        'CREATE TABLE mealtime_food(MealTimeFoodId INTEGER PRIMARY KEY, MealId_FK INTEGER, IngredientId_FK INTEGER, DayMealTimeId_FK, FOREIGN KEY (MealId_FK) REFERENCES meal(MealId), FOREIGN KEY (IngredientId_FK) REFERENCES ingredient(IngredientId), FOREIGN KEY (DayMealTimeId_FK) REFERENCES day_mealtime(DayMealTimeId))');
 
-    //Adding Seed data 
+    //Adding Seed data
     await SeedData.seedDaysTableData(db);
     await SeedData.seedMealTimeTableData(db);
     await SeedData.seedDayMealTimeTableData(db);
@@ -34,5 +42,11 @@ class DBHelper {
     final db = await DBHelper.database();
     return db.query(table);
   }
-}
 
+  //will need to change the method name as it is specific to getting data for - day_mealtime
+  //Or you can put the SQL string in a seperate file and call those somehow just to make the code a bit more modular
+  static Future<List<Map<String, dynamic>>> getDataRawQuery(String queryName) async {
+    final db = await DBHelper.database();
+    return db.rawQuery(RawSQLQueryHelper.getSQLQueryString(queryName));
+  }
+}
---
+++ meal_planner/lib/helpers/raw_sql_query_helper.dart
@@ -0,0 +1,15 @@
+//import 'package:meal_planner/providers/day_mealtime_provider.dart';
+
+class RawSQLQueryHelper{
+
+  static const String dayMealtimeTableData = 'SELECT dmt.DayMealTimeId, d.DayId, d.DayName, mt.MealTimeId, mt.MealTimeName FROM day_mealtime dmt JOIN day d ON dmt.DayId_FK = d.DayId JOIN mealtime mt ON dmt.MealTimeId_FK = mt.MealTimeId';
+
+  static String getSQLQueryString(String queryName) {
+    switch(queryName){
+      case 'dayMealtimeTableData':
+        return dayMealtimeTableData;
+      default:
+        return '';
+    }
+  }
+}
